edf8442c5f9cbb15c7d87eaf4228a60371ae1582
slavcopost <slavcopost@gmail.com>
2013-05-08 20:33:04 +0300

    Implement unlink models

--- Linkage.php
+++ Linkage.php
@@ -21,6 +21,7 @@ class Linkage extends CBehavior
 	const ERROR_PK_EMPTY = 'Unable to link models: the primary key of %s is null.';
 	const ERROR_SAVE_ASSN = 'Unable to link models: cannot save association.';
 	const ERROR_SAVE = 'Unable to save %s model.';
+	const ERROR_DELETE = 'Unable to delete %s model.';
 	const ERROR_UNSUPPORT_RELATION = 'Unsupport relation type.';
 
 	/**
@@ -107,6 +108,83 @@ class Linkage extends CBehavior
 	}
 
 	/**
+	 * @param string $name the name of the relationship
+	 * @param ActiveRecord $model the model to be unlinked with the current one
+	 * @param boolean $delete whether to delete the model or unset foreign key.
+	 * @return CActiveRecord
+	 * @throws CException if the method is unable to unlink two models.
+	 */
+	public function unlink($name, $model, $delete = false)
+	{
+		$owner = $this->getOwner();
+		$relation = $this->getActiveRelation($name);
+
+		if ($relation instanceof CManyManyRelation) {
+			list ($pk, $fk) = $relation->getJunctionForeignKeys();
+			$columns = array();
+			$columns[$pk] = $owner->primaryKey;
+			$columns[$fk] = $model->primaryKey;
+
+			$pivotTable = $relation->getJunctionTableName();
+			$criteria = new CDbCriteria();
+			$criteria->addColumnCondition($columns);
+			$command = $owner->getCommandBuilder()->createDeleteCommand($pivotTable, $criteria);
+			if(!$command->execute()) {
+				throw new CException(sprintf(self::ERROR_SAVE_ASSN, $pivotTable));
+			}
+		} elseif ($relation->through !== null) {
+			$bridgeRelation = $this->getActiveRelation($relation->through);
+			$columns = array();
+			$columns[$bridgeRelation->foreignKey] = $owner->primaryKey;
+			$columns[$relation->foreignKey] = $model->primaryKey;
+
+			/** @var CActiveRecord $bridgeModel */
+			$bridgeModel = CActiveRecord::model($bridgeRelation->className)
+				->findByAttributes($columns);
+
+			if ($delete && !$bridgeModel->delete()) {
+				throw new CException(sprintf(self::ERROR_DELETE, get_class($bridgeModel)));
+			} else {
+				foreach ($columns as $key => $value) {
+					$bridgeModel->$key = null;
+				}
+				if (!$bridgeModel->save(false)) {
+					throw new CException(sprintf(self::ERROR_SAVE, get_class($bridgeModel)));
+				}
+			}
+		} else {
+			if ($delete && !$model->getIsNewRecord() && !$model->delete()) {
+				throw new CException(sprintf(self::ERROR_DELETE, get_class($model)));
+			} else {
+				foreach ($this->getRelationKeys($relation) as $fk => $pk) {
+					$model->$fk = null;
+				}
+				if (!$model->save(false)) {
+					throw new CException(sprintf(self::ERROR_SAVE, get_class($model)));
+				}
+			}
+		}
+
+		// Update lazily loaded related objects
+		if (!$relation instanceof CHasManyRelation) {
+			unset($owner->$name);
+		} elseif (($index = $relation->index) !== null) {
+			unset($owner->$name[$index]);
+		} else {
+			$models = array();
+			foreach ($owner->getRelated($name) as $related) {
+				if (!$model->equals($related)) {
+					array_push($models, $related);
+				}
+			}
+			unset($owner->$name);
+			$owner->addRelatedRecord($name, $models, false);
+		}
+
+		return $owner;
+	}
+
+	/**
 	 * @param string $name
 	 * @return CActiveRelation|CHasOneRelation|CHasManyRelation|CManyManyRelation
 	 * @throws CException if the method is unable to link two models.
@@ -118,5 +196,12 @@ class Linkage extends CBehavior
 			throw new CException(sprintf(self::ERROR_UNSUPPORT_RELATION, get_class($relation)));
 		}
 		return $relation;
+	}
+
+	private function getRelationKeys($relation)
+	{
+		return array(
+			$relation->foreignKey => $this->getOwner()->getPrimaryKey()
+		);
 	}
 }
